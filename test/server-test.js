063e058b0ab07597f4f2eee248a9b5ea2e488e02
joshuajhun <joshuajhun@gmail.com>
2016-02-29 23:49:22 -0700

    added a before action to the test suite to change the test port

--- test/server-test.js
+++ test/server-test.js
@@ -1,5 +1,16 @@
 const assert = require('assert');
 const app = require('../server');
+before(done => {
+  this.port = 9876;
+  this.server = app.listen(this.port, (err, result) => {
+    if (err) { return done(err); }
+    done();
+  });
+});
+
+after(() => {
+  this.server.close();
+});
 
 describe('Server', function(){
 
